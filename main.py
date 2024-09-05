4a4adbdafd0462d9974243af468b92ce69572b6a
Bevan <71739185+bevan654@users.noreply.github.com>
2024-09-06 02:40:54 +0930

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ class Main:
                 continue
 
             if response.status_code == 200:
-                response = response.content
+                response = response.content.strip().lower()
                 if CURRENT_VERSION != response:
                     self.LOG('[CHECK_FOR_UPDATES] New updates available!')
                     self.download_update(response)
